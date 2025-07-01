b217b8ee7d831514c8f5a5d311b3b4b0044675f6
Guilherme <guilherme.limasantiago@gmail.com>
2025-07-01 20:41:15 -0300

    test: coverage model

--- src/models/Region.ts
+++ src/models/Region.ts
@@ -2,19 +2,16 @@ import { Schema, model } from "mongoose";
 
 const regionSchema = new Schema({
   name: { type: String, required: true },
-  geometry: {
-    type: {
+  type: {
       type: String,
       enum: ["Polygon"],
       required: true,
     },
-    coordinates: {
-      type: [[[Number]]],
+  coordinates: {
+      type: [[[String]]],
       required: true,
     },
-  },
 });
 
-regionSchema.index({ geometry: "2dsphere" });
 
 export const Region = model("Region", regionSchema);
