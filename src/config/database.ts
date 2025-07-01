190ee8952c458f4e10f9f7b2a2dbf25fd9b630a8
Guilherme <guilherme.limasantiago@gmail.com>
2025-07-01 18:05:23 -0300

    fix: import modules commonjs

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/ozmap');
-    console.log('Database connected');
+    await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost:27017/ozmap",
+    );
+    console.log("Database connected");
   } catch (error) {
-    console.error('Error to connect Database', error);
+    console.error("Error to connect Database", error);
     process.exit(1);
   }
 };
