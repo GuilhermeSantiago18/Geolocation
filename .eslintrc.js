ecfda1b24d636a5a27a3a9687d3e6f0151dcd598
Guilherme <guilherme.limasantiago@gmail.com>
2025-07-01 17:58:14 -0300

    chore: remove wrong ESLint config file

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {},
+};
