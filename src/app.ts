4f91f42309e64513052f7297c4892a77bf015c5c
Guilherme <guilherme.limasantiago@gmail.com>
2025-07-03 12:36:10 -0300

    chore: setup for apply translate at middleware

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,18 @@ import swaggerUi from "swagger-ui-express";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 import YAML from "yamljs";
 import { OpenAPIV3 } from "openapi-types";
+import i18nextMiddleware from "i18next-http-middleware";
+import i18n from "./i18n";
+
+const swaggerDocument = YAML.load(
+  "./src/docs/swagger.yaml",
+) as OpenAPIV3.Document;
 
 const app = express();
 
 app.use(express.json());
 
-const swaggerDocument = YAML.load(
-  "./src/docs/swagger.yaml",
-) as OpenAPIV3.Document;
+app.use(i18nextMiddleware.handle(i18n));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
