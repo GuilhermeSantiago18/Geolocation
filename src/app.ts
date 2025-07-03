c36eb8c9cb5171135de411abef84370dbf35b4b1
Guilherme <guilherme.limasantiago@gmail.com>
2025-07-02 23:12:24 -0300

    chore: new route for swagger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import RegionRoutes from "./routes/regionRoutes";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./docs/swaggerConfig";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
@@ -7,6 +9,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/region", RegionRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(errorMiddleware);
 
